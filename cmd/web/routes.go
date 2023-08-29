0d6e377780ad13a71b30cbccc407b89727005656
Umer Farooq <umer@coffeebeans.io>
2023-08-29 17:27:23 +0530

    test commit

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -14,7 +14,6 @@ func routes(app *config.AppConfig) http.Handler {
 	//
 	//mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
 	//mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
-
 	mux := chi.NewRouter()
 
 	// Middleware
